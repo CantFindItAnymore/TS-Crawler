8477fc760867c99f664b26b552a9f71aca6a16a3
Ran Jun <ranjun@ybtcn.cn>
2020-11-12 15:19:54 +0800

    爬取落网音乐数据

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -1,71 +1,83 @@
-import cheerio from "cheerio";
-import fs from "fs";
-import { Analyze } from "./crawler";
+import cheerio from 'cheerio'
+import fs from 'fs'
+import { Analyze } from './crawler'
 
 interface Course {
-  title: string;
-  count: number;
+	id: string
+	title: string
+	img: string
+	alt: string
 }
 
 interface CourseResult {
-  date: number;
-  data: Course[];
+	date: number
+	data: Course[]
 }
 
 interface Content {
-  [propName: number]: Course[]
+	[propName: number]: Course[]
 }
 
 export default class Analyzer implements Analyze {
+	private static instance: Analyzer
 
-  private static instance: Analyzer
+	static getInstance() {
+		if (!Analyzer.instance) {
+			Analyzer.instance = new Analyzer()
+		}
+		return Analyzer.instance
+	}
 
-  static getInstance() {
-    if(!Analyzer.instance) {
-      Analyzer.instance = new Analyzer()
-    }
-    return Analyzer.instance
-  }
+	// 拿到www.dell-lee.com中的我们需要的数据
+	private getCourseInfo = (html: string) => {
+		const $ = cheerio.load(html)
+		const courseItems = $('.thumbnail')
+		const courseInfos: Course[] = []
+		courseItems.map((index, element) => {
+			const img = $(element).find('img')[0].attribs.src
+			const alt = $(element).find('img')[0].attribs.alt
+			const a = $(element).find('a')
+			const title = a.eq(1).text()
+			const id = a.eq(1)[0].attribs.href.split('/')[1]
+			courseInfos.push({
+				id,
+				img,
+				alt,
+				title,
+			})
+		})
 
+		return courseInfos
+	}
 
-  // 拿到www.dell-lee.com中的我们需要的数据
-  private getCourseInfo = (html: string) => {
-    const $ = cheerio.load(html);
-    const courseItems = $(".course-item");
-    const courseInfos: Course[] = [];
-    courseItems.map((index, element) => {
-      const desc = $(element).find(".course-desc");
-      const title = desc.eq(0).text();
-      const count = parseInt(desc.eq(1).text().split("：")[1], 10);
-      courseInfos.push({
-        title,
-        count,
-      });
-    });
-    const result = {
-      date: new Date().getTime(),
-      data: courseInfos,
-    };
-    return result;
-  };
+	// 转化为我们需要的数据结构
+	private generateJsonContent = (courseInfo: Course[], filePath: string) => {
+		let fileContent: Course[] = []
+		let exitedIds: string[] = []
+		if (fs.existsSync(filePath)) {
+			fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+		}
 
-  // 转化为我们需要的数据结构
-  private generateJsonContent = (courseInfo: CourseResult, filePath: string) => {
-    let fileContent: Content = {};
-    if (fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    }
+    // 有的就不要了
+		fileContent.map(child => {
+			exitedIds.push(child.id)
+    })
+    console.log(exitedIds)
+		courseInfo.map(item => {
+			if (!exitedIds.includes(item.id)) {
+				fileContent.push(item)
+			}
+		})
 
-    fileContent[courseInfo.date] = courseInfo.data;
-    return fileContent;
-  };
+		return fileContent
+	}
 
-  public analyze = (html: string, filePath: string) => {
-    const courseInfo = this.getCourseInfo(html)
-    const jsonContent = this.generateJsonContent(courseInfo, filePath);
-    return JSON.stringify(jsonContent)
-  };
+	public analyze = (html: string, filePath: string) => {
+		const courseInfo = this.getCourseInfo(html)
+		const jsonContent = this.generateJsonContent(courseInfo, filePath)
+		return JSON.stringify(jsonContent)
+	}
 
-  // 单例模式 不能被外部实例化
-  private constructor() {}
+	// 单例模式 不能被外部实例化
+	private constructor() {}
 }
--- src/crawler.ts
+++ src/crawler.ts
@@ -1,45 +1,52 @@
 // 优化：现在该文件只管爬虫和存储文件，分析逻辑移到了analyzer.js里面
 import fs from 'fs'
 import path from 'path'
-import superagent from "superagent";
+import superagent from 'superagent'
 
 import Analyzer from './analyzer'
 
 export interface Analyze {
-  analyze: (html: string, filePath: string) => string
+	analyze: (html: string, filePath: string) => string
 }
 
 class Crawler {
-  // private secret = "x3b174jsx";
-  // private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-  // private url = 'http://ybtcn.cn'
-  private filePath = path.resolve(__dirname, "../data/course.json");
+	private filePath = path.resolve(__dirname, '../data/luo.json')
 
-  // 获取dom
-  private getRawHtml = async () => {
-    const result = await superagent.get(this.url);
-    return result.text;
-  };
+	// 获取dom
+	private getRawHtml = async () => {
+		const result = await superagent.get(this.url)
+		return result.text
+	}
 
-  // 写入文件
-  private writeJsonContent = (fileContent: string) => {
-    fs.writeFileSync(this.filePath, fileContent);
-  };
+	// 写入文件
+	private writeJsonContent = (fileContent: string) => {
+		fs.writeFileSync(this.filePath, fileContent)
+	}
 
-  private crawler = async () => {
-    const html = await this.getRawHtml();
-    const jsonContent = await this.analyzer.analyze(html, this.filePath);
-    this.writeJsonContent(jsonContent);
-  };
+	private crawler = async () => {
+		const html = await this.getRawHtml()
+		const jsonContent = await this.analyzer.analyze(html, this.filePath)
+		this.writeJsonContent(jsonContent)
+	}
 
-  constructor(private url: string, private analyzer: Analyze) {
-    this.crawler();
-  }
+	constructor(private url: string, private analyzer: Analyze) {
+		this.crawler()
+	}
 }
 
-const secret = "x3b174jsx";
-const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-// const analyzer = new Analyzer()
-const analyzer = Analyzer.getInstance();
+const start = (url: string) => {
+	const analyzer = Analyzer.getInstance()
 
-const crawler = new Crawler(url, analyzer);
+	const crawler = new Crawler(url, analyzer)
+}
+
+start(`https://www.luoow.com`)
+start(`https://www.luoow.com/1_100.html`)
+start(`https://www.luoow.com/101_200.html`)
+start(`https://www.luoow.com/201_300.html`)
+start(`https://www.luoow.com/301_400.html`)
+start(`https://www.luoow.com/401_500.html`)
+start(`https://www.luoow.com/501_600.html`)
+start(`https://www.luoow.com/601_700.html`)
+start(`https://www.luoow.com/701_800.html`)
+start(`https://www.luoow.com/801_900.html`)
